1d6c33ee73aaf07263c49d932f46788e3e37deec
HUGE | Lukas Mairl <lmairl@lmairl-ny-06186.ad.hugeinc.com>
2014-11-11 10:02:00 -0500

    Basic HTML5 Video Player

---
+++ Video.js
@@ -0,0 +1,24 @@
+<div class="video">
+  <video data-videoid="">
+  <p>Your browser doesn't support HTML5 video.</p>
+  <div class="video-controls">
+      <button type="button" class="play-pause">Play</button>
+      <div class="progress-container">
+        <span class="current-time"></span>
+        <div class="progress-bar">
+          <div class="button-holder">
+            <div class="progress-button"></div>
+          </div>
+          <div class="progress"></div>
+          <div class="buffer-bar"></div>
+        </div>
+        <span class="duration"></span>
+      </div>
+      <button type="button" class="mute">Mute</button>
+      <button type="button" class="fullscreen">Full-Screen</button>
+      <div class="volume-bar">
+        <div class="volume-button"></div>
+        <div class="volume"></div>
+      </div>
+  </div>
+</div>
